0f03898bcccc3187780c7ba77539885b80f9f5b6
AI171826 <AI171826@P009D017.reigate.ac.uk>
2018-06-07 10:21:51 +0100

    Added prime number finding algorithm

--- Benchmarks/src/benchmarks/Benchmarks.java
+++ Benchmarks/src/benchmarks/Benchmarks.java
@@ -4,9 +4,34 @@ import java.util.*;
 
 public class Benchmarks {
 
-    
+    Random rand = new Random();
 
     public static void main(String[] args) {
-        
+        long primeTime = primeFinder(500000);
+        System.out.println(primeTime + "nanoseconds");
+    }
+    
+    public static long primeFinder(int numLimit){
+        ArrayList<Integer> primeList = new ArrayList<>();
+        long startingTime = System.nanoTime();
+        for (int num = 1; num < numLimit; num++) {
+            if ((num % 5000) == 0){
+                System.out.println("Progress: " + (num / 5000) + "%");
+            }
+            int half_num = num / 2;
+            int i = 2;
+            ArrayList<Integer> factors = new ArrayList<>();
+            while(i <= half_num){
+                if ((num % i) == 0){
+                    factors.add(i);
+                }
+                i++;
+            }
+            if (factors.isEmpty()){
+                primeList.add(num);
+            }
+        }
+        long elapsedTime = System.nanoTime() - startingTime;
+        return elapsedTime;
     }
 }
